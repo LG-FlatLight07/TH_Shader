5d1aa59bba5fbd98077cb6f48adb251812b2742c
たりお <ahikari1621@gmail.com>
2024-01-01 18:27:01 +0900

    OutLine

---
+++ Source/THShader/THShader.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "THShader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, THShader, "THShader" );
---
+++ Source/THShader/THShaderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "THShaderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THSHADER_API ATHShaderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
